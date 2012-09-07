9e64e144103975d15d4276b8e2377c1f57abab6f
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2012-09-07 12:36:55 +0200

    Add .profile with basic PATH setup
    This way Gnome Shell picks up the PATH variable and looks for programs
    in there.

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ install()
     ln -fvs "$(pwd)/$1" "$2"
 }
 
-for f in bashrc bash_aliases bash_functions vimrc gvimrc; do
+for f in bashrc bash_aliases bash_functions vimrc gvimrc profile; do
     install $f ~/.$f
 done
