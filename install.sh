481568aa90cd5897f762e868bd866e6def6d3c7d
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2016-06-14 12:48:02 +0200

    Install term_colors file

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ install()
     ln -fvs "$(pwd)/$1" "$2"
 }
 
-for f in bashrc bash_aliases bash_functions vimrc gvimrc profile vim/colors/oblivion.vim; do
+for f in bashrc bash_aliases bash_functions vimrc gvimrc profile vim/colors/oblivion.vim term_colors; do
     mkdir -pv "$(dirname "$f")"
     install $f ~/.$f
 done
